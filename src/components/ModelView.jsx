bf603c5b193ef50cf4af7fbfd165c8af34884bc2
Zakary <chauve.zakary@gmail.com>
2024-09-03 22:05:19 +0200

    Added View, camera and lights, with a suspense from React

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { PerspectiveCamera, View } from '@react-three/drei'
+import React, { Suspense } from 'react'
+import Lights from './Lights'
+import { div } from 'three/webgpu'
 
-const ModelView = () => {
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
   return (
-    <div>ModelView</div>
-  )
+    <View index={index} id={gsapType} className={`border-2 w-full h-full ${index === 2} ? 'right-[-100%] : '`}>
+
+    //ambient light
+    <ambientLight intensity={0.3}/>
+
+    <PerspectiveCamera makeDefault position={[0, 0, 4]}/>
+
+    <Lights />
+
+    <Suspense fallback={<div>Loading</div>} />
+
+    </View>
+)
 }
 
 export default ModelView
