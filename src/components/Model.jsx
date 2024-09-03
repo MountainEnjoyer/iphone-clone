f03f08f4643b5e2526d016cefe2631639a2b6905
Zakary <chauve.zakary@gmail.com>
2024-09-03 20:51:36 +0200

    Canvas for the models started, added the displayed colors and the size

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,6 +6,7 @@ import { yellowImg } from '../utils';
 import * as THREE from 'three';
 import { View } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { models, sizes } from '../constants';
 
 const Model = () => {
     const [size, setSize] = useState('small');
@@ -82,6 +83,23 @@ const Model = () => {
                 <p className='text-sm font-light text-center mb-5'>
                     {model.title}
                 </p>
+                <div className="flex-center">
+                    <ul className='color-container'>
+                        {models.map((item, i) => (
+                            <li key={i} className='w-6 h-6 rounded-full mx-2 cursor-pointer' style={{backgroundColor: item.color[0]}} onClick={() => setModel(item)}>
+                                
+                            </li>
+                        ))}
+                    </ul>
+
+                    <button className="size-btn-container">
+                        {sizes.map(({ label, value }) => (
+                            <span key={label} className='size-btn' style={{backgroundColor: size === value ? 'white' : 'transparent'}}>
+                                {label}
+                            </span>
+                        ))}
+                    </button>
+                </div>
             </div>
 
             </div>
