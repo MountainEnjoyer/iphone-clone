40af14443c945a85bbcd09dcfbe38c3176e219d3
Zakary <chauve.zakary@gmail.com>
2024-09-08 23:24:52 +0200

    Added a Loader file to customize the Loading warning with watherver i'll want

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -4,6 +4,7 @@ import Lights from './Lights';
 import { div } from 'three/webgpu';
 import IPhone from './IPhone';
 import * as THREE from 'three';
+import Loader from './Loader';
 
 
 const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
@@ -20,7 +21,7 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size
     <OrbitControls makeDefault ref={controlRef} enableZoom={false} enablePan={false} rotateSpeed={0.4} target={new THREE.Vector3(0,0,0)} onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}/>
 
     <group ref={groupRef} name={`${index === 1 ? 'small' : 'large'}`} position={[0, 0, 0]}>
-        <Suspense fallback={<Html><div>Loading</div></Html>}>
+        <Suspense fallback={<Loader />}>
             <IPhone 
                 scale={index === 1 ? [15,15,15] : [17,17,17]}
                 item={item}
