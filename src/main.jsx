b130dff3a6158daa5bf423adf8dce8e3a81ed07c
Zakary <chauve.zakary@gmail.com>
2024-09-09 10:14:58 +0200

    Added Sentry to track the user's experience and get metrics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,29 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://ff28572d8a8b167e81b84b9846b8538f@o4507921370185728.ingest.de.sentry.io/4507921377853520",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
