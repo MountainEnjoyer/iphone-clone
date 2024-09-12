79d65b9494f1e25d0440f24d289219b0b2b1cd2a
Zakary <chauve.zakary@gmail.com>
2024-09-12 11:02:23 +0200

    Style changes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,11 +2,23 @@ import { useGSAP } from '@gsap/react'
 import React, { useRef } from 'react'
 import { animateWithGsap } from '../utils/animations'
 import { explore1Img, explore2Img, exploreVideo } from '../utils'
+import gsap from 'gsap'
 
 const Features = () => {
     const videoRef = useRef();
 
     useGSAP(() => {
+        gsap.to('#exploreVideo', {
+            scrollTrigger: {
+              trigger: '#exploreVideo',
+              toggleActions: 'play pause reverse restart',
+              start: '-10% bottom',
+            },
+            onComplete: () => {
+                videoRef.current.play();
+            }
+        })
+
         animateWithGsap('#features_title', {y:0, opacity:1});
         animateWithGsap('.g_grow', {scale: 1, opacity: 1, ease: 'power1'}, {scrub: 5.5});
         animateWithGsap('.g_text' , {y:0, opacity:1, ease:'power2.inOut', duration:1});
@@ -32,13 +44,13 @@ const Features = () => {
             </div>
             <div className='flex-center flex-col sm:px-10'>
                 <div className='relative h-[50vh] w-full flex items-center'>
-                    <video playsInline id='exploreVIdeo' className='w-full h-full object-cover object-center' preload='none' muted autoPlay ref={videoRef}>
+                    <video playsInline id='exploreVideo' className='w-full h-full object-cover object-center' preload='none' muted autoPlay ref={videoRef}>
                         <source src={exploreVideo} type="video/mp4"/>
                     </video>
                 </div>
 
                 <div className="flex flex-col w-full relative">
-                    <div className="features-video-container">
+                    <div className="feature-video-container">
                         <div className="overflow-hidden flex-1 h-[50vh]">
                             <img src={explore1Img} alt="titanium" className='feature-video g_grow'/>
                         </div>
@@ -46,6 +58,7 @@ const Features = () => {
                             <img src={explore2Img} alt="titanium2" className='feature-video g_grow'/>
                         </div>
                     </div>
+
                     <div className="feature-text-container">
                         <div className="flex-1 flex-center">
                             <p className="feature-text g_text">
