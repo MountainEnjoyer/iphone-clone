511cc9480d3827388af9102cfa641ace53e8c14e
Zakary <chauve.zakary@gmail.com>
2024-09-12 10:40:27 +0200

    Added the last 2 img with animations

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,13 +1,14 @@
 import { useGSAP } from '@gsap/react'
 import React, { useRef } from 'react'
 import { animateWithGsap } from '../utils/animations'
-import { exploreVideo } from '../utils'
+import { explore1Img, explore2Img, exploreVideo } from '../utils'
 
 const Features = () => {
     const videoRef = useRef();
 
     useGSAP(() => {
         animateWithGsap('#features_title', {y:0, opacity:1})
+        animateWithGsap('.g_grow', {scale: 1, opacity: 1, ease: 'power1'}, {scrub: 5.5})
     }, [])
 
   return (
@@ -32,7 +33,18 @@ const Features = () => {
                     <video playsInline id='exploreVIdeo' className='w-full h-full object-cover object-center' preload='none' muted autoPlay ref={videoRef}>
                         <source src={exploreVideo} type="video/mp4"/>
                     </video>
-                </div>                
+                </div>
+
+                <div className="flex flex-col w-full relative">
+                    <div className="features-video-container">
+                        <div className="overflow-hidden flex-1 h-[50vh]">
+                            <img src={explore1Img} alt="titanium" className='feature-video g_grow'/>
+                        </div>
+                        <div className="overflow-hidden flex-1 h-[50vh]">
+                            <img src={explore2Img} alt="titanium2" className='feature-video g_grow'/>
+                        </div>
+                    </div>
+                </div>
            </div>
         </div>
     </section>
